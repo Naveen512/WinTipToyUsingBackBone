5ee623e37168f8dd0661311999ecda770fbb6251
Naveen512 <bommidinaveen@gmail.com>
2016-02-03 22:19:30 +0530

    final commit

--- WingTipToysBackBoneView/Scripts/BackBoneScripts/View.js
+++ WingTipToysBackBoneView/Scripts/BackBoneScripts/View.js
@@ -122,14 +122,24 @@ var WingTipToy = WingTipToy || {};
         },
         events:{
             'click #addToCart': 'DisplayShopingChart',
+            'click #deleteItem': 'deleteShopingCartItem',
+            'click #confirmDelete': 'confirmDeletemethod',
+            'click #editTemplate': 'editShopingCartItem',
+            'click .glyphicon-plus,.glyphicon-minus': 'changeQunatity',
+            'click #confirmEdit': 'confirmEditMethod'
         },
-        DisplayShopingChart: function () {
+        DisplayShopingChart: function (e,type) {
+            if (type)
+            {
+                //nothing to do here
+            }
+            else{
             var model;
             var quan = this.collection.where({ Name: $('.media-left h3').text() }).length;
             if (quan === 1) {
                 updateModel = this.collection.findWhere({ Name: $('.media-left h3').text() });
                 this.collection.remove(updateModel);
-                 model = new WingTipToy.ModelCategory.ShopingCart({
+                model = new WingTipToy.ModelCategory.ShopingCart({
                     ProductID: updateModel.attributes.ProductID,
                     Name: updateModel.attributes.Name,
                     Price: updateModel.attributes.Price,
@@ -142,7 +152,7 @@ var WingTipToy = WingTipToy || {};
             else {
 
                 //shopID = shopID + 1;
-                 model = new WingTipToy.ModelCategory.ShopingCart({
+                model = new WingTipToy.ModelCategory.ShopingCart({
                     ProductID:Number($('.media-body span')[1].innerHTML),
                     Name: $('.media-left h3').text(),
                     Price: $('.media-body span')[0].innerHTML,
@@ -152,6 +162,7 @@ var WingTipToy = WingTipToy || {};
 
                 this.collection.add(model);
             }
+        }
             //this.collection.comparator = "ID";
             var templateName = "http://localhost:5342/Templates/ShopingCart.html";
             $('#teplateParentHolder').html('');
@@ -172,8 +183,108 @@ var WingTipToy = WingTipToy || {};
             var outputHtml = template({ ShopingCart: this.collection.toJSON() });
             $("#templateSuperParentHolder").html('');
             $("#templateSuperParentHolder").append(outputHtml);
+            if (!type) {
+
             $("tr[data-val='" + model.attributes.ProductID + "']").addClass('success');
+            }
         },
+        
+        deleteShopingCartItem: function (e) {
+            var id = Number(e.currentTarget.closest('tr').children[1].innerHTML);
+            //e.currentTarget.closest('tr').remove();
+
+            //this.collection.findWhere({ ProductID: id }).remove();
+            var modelToDelete = this.collection.findWhere({ ProductID: id });
+            var templateName = "http://localhost:5342/Templates/DeleteModelPopup.html";
+            $.ajax({
+                type: 'GET',
+                url: templateName,
+                async: false,
+                'success': function (response) {
+                    $(".modal-content").html('');
+                    $(".modal-content").append(response);  
+                }
+            });
+
+            $("#ModalProductID").text(modelToDelete.get('ProductID'));
+            $("#ModalName").text(modelToDelete.get('Name'));
+            $("#ModalPrice").text(modelToDelete.get('Price'));
+            $("#ModelQuality").text(modelToDelete.get('Quality'));
+            $("#ModalItemTotal").text(modelToDelete.get('ItemTotal'));
+            $(".bs-example-modal-lg").modal('show');
+            //this.collection.remove(modelToDelete);
+            //this.DisplayShopingChart('',"deleteShopingCartItem");
+        },
+        confirmDeletemethod: function () {
+            var modelToDelete = this.collection.findWhere({ ProductID: Number($("#ModalProductID").text()) });
+           this.collection.remove(modelToDelete);
+           this.DisplayShopingChart('', "deleteShopingCartItem");
+           $(".bs-example-modal-lg").modal('hide');
+           $(".modal-content").html('');
+        },
+        editShopingCartItem: function (e) {
+            var id = Number(e.currentTarget.closest('tr').children[1].innerHTML);
+            var modelToEdit = this.collection.findWhere({ ProductID: id });
+            var templateName = "http://localhost:5342/Templates/EditModelPopUp.html";
+            $.ajax({
+                type: 'GET',
+                url: templateName,
+                async: false,
+                'success': function (response) {
+                    $(".modal-content").html('');
+                    $(".modal-content").append(response);
+                }
+            });
+
+            $("#ModalProductID").val(modelToEdit.get('ProductID'));
+            $("#ModalName").val(modelToEdit.get('Name'));
+            $("#ModalPrice").val(modelToEdit.get('Price'));
+            $("#ModelQuality").val(modelToEdit.get('Quality'));
+            $("#ModalItemTotal").val(modelToEdit.get('ItemTotal'));
+            $(".bs-example-modal-lg").modal('show');
+        },
+        changeQunatity: function (e) {
+            console.log();
+            if(e.currentTarget.classList.contains('glyphicon-plus'))
+            {
+                if(Number($("#ModelQuality").val())>=1)
+                {
+                    $("#ModelQuality").val(Number($("#ModelQuality").val()) + 1);
+                }
+            }
+            else if (e.currentTarget.classList.contains('glyphicon-minus')) {
+                if (Number($("#ModelQuality").val()) > 1) {
+                    $("#ModelQuality").val(Number($("#ModelQuality").val()) - 1);
+                }
+            }
+        },
+        confirmEditMethod: function () {
+            var id = Number($("#ModalProductID").val());
+            var editMethod = this.collection.findWhere({ ProductID: id });
+            editMethod.set('Quality', $("#ModelQuality").val());
+
+            var templateName = "http://localhost:5342/Templates/ShopingCart.html";
+            $('#teplateParentHolder').html('');
+            //$.get(templateName, function (response) {
+
+            //    $('#teplateParentHolder').append(response);
+            //});
+            $.ajax({
+                type: 'GET',
+                url: templateName,
+                async: false,
+                'success': function (response) {
+                    $('#teplateParentHolder').append(response);
+                }
+            })
+            var source = $('#ShpotingCartTemplate').html();
+            var template = Handlebars.compile(source);
+            var outputHtml = template({ ShopingCart: this.collection.toJSON() });
+            $("#templateSuperParentHolder").html('');
+            $("#templateSuperParentHolder").append(outputHtml);
+            $(".bs-example-modal-lg").modal('hide');
+            $(".modal-content").html('');
+        }
         
     });
 
